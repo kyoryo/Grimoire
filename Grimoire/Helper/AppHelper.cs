537645ea44e793676fd2ad1ff41ce223fb52ad72
Kyoryo Ishigami <franz.rio19@gmail.com>
2016-10-19 21:35:20 +0700

    add initial run

---
+++ Grimoire/Helper/AppHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Grimoire.Helper
+{
+    public class AppHelper
+    {
+        public static string GetFontFile()
+        {
+            return ConfigurationManager.AppSettings["font"];
+        }
+    }
+}
--- Grimoire/Program.cs
+++ Grimoire/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Grimoire.Helper;
+using RLNET;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +8,32 @@ using System.Threading.Tasks;
 
 namespace Grimoire
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static readonly int _screenWidth = 100;
+        private static readonly int _screenHeight = 80;
+        private static readonly int _charWidth = 8;
+        private static readonly int _charHeight = 8;
+        private static RLRootConsole _rootConsole;
+        static void Main()
         {
+            string fontFile = AppHelper.GetFontFile();
+            //string fontFile = "terminal8x8.png";
+            string title = "Tes Level 1";
+
+            _rootConsole = new RLRootConsole(fontFile, _screenWidth, _screenHeight, _charWidth, _charHeight, 1f, title);
+            _rootConsole.Update += OnRootConsoleUpdate;
+            _rootConsole.Render += OnRootConsoleRender;
+            _rootConsole.Run();
+
+        }
+        private static void OnRootConsoleUpdate(object sender, UpdateEventArgs e)
+        {
+            _rootConsole.Print(10, 10, "tes", RLColor.White);
+        }
+        private static void OnRootConsoleRender(object sender, UpdateEventArgs e)
+        {
+            _rootConsole.Draw();
         }
     }
 }
