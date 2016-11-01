4cf24e1294be201904468cae4c19d56449f4db28
Kyoryo Ishigami <franz.rio19@gmail.com>
2016-11-02 00:51:07 +0700

    leftover from 68d787a

---
+++ Grimoire.UI/Cells/CellSymbol.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grimoire.UI.Cells
+{
+    public class CellSymbol
+    {
+        public static readonly char floor = '.';
+        public static readonly char wall = '#';
+    }
+}
